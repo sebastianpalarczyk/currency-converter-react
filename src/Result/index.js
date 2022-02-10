76968f760f2fad9374fc2a825e610a6a46ed9a2b
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-02-10 18:06:14 +0100

    added components: Container, Form, Result

---
+++ src/Result/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Result = (props) => (
+    <div>
+        <p className="paragraph--hidden"></p>
+        <p className="paragraph--hidden"></p>
+        <p className="paragraph--hidden"></p>
+    </div>
+)
+
+export default Result;
