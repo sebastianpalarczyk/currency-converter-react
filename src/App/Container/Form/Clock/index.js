9e5db12e330f586f45e8f2d626c9336b8d14b52f
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-02-20 15:04:16 +0100

    added custom hook useCurrentDate

--- src/App/Container/Form/Clock/index.js
+++ src/App/Container/Form/Clock/index.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
-
-const formaDate = (date) => date.toLocaleDateString(undefined, {
-    weekday: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    day: "numeric",
-    month: "long"
-});
+import React from "react";
+import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date());
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
         <>
-        Dzisiaj jest{" "} {formaDate(date)}
+            Dzisiaj jest{" "} {date}
         </>
     )
 }
---
+++ src/App/Container/Form/Clock/useCurrentDate.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from "react";
+
+export const useCurrentDate = () => {
+    const [date, setDate] = useState(new Date());
+
+    const currentDate = useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const formaDate = (date) => date.toLocaleDateString(undefined, {
+        weekday: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day: "numeric",
+        month: "long"
+    });
+
+   return formaDate(date);
+}
